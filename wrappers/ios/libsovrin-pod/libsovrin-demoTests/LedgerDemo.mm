fb2b8eced810e36ee21a6c1b5d95d0d4b9098dfd
Kirill Neznamov <kirill.neznamov@dsr-company.com>
2017-05-25 16:17:27 +0300

    more functionality for anoncreds complex test

--- wrappers/ios/libsovrin-pod/libsovrin-demoTests/LedgerDemo.mm
+++ wrappers/ios/libsovrin-pod/libsovrin-demoTests/LedgerDemo.mm
@@ -2,9 +2,7 @@
 //  LedgerDemo.m
 //  libsovrin-demo
 //
-//  Created by Kirill Neznamov on 15/05/2017.
-//  Copyright © 2017 Kirill Neznamov. All rights reserved.
-//
+
 
 #import <XCTest/XCTest.h>
 #import "PoolUtils.h"
